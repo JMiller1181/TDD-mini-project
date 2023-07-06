137030b157142877950a46b5b9a7e646985e74c8
Jacob Miller <millerjr1181@gmail.com>
2023-07-06 12:15:50 -0400

    initial commit

---
+++ src/main/java/com/fs104/tddminiproj/TddMiniProjApplication.java
@@ -0,0 +1,13 @@
+package com.fs104.tddminiproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddMiniProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddMiniProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fs104/tddminiproj/TddMiniProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs104.tddminiproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddMiniProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
